85923a6e2816f54e1f964a95811d747fd614a45f
Ravikumar1810 <nkravikumar18@gmail.com>
2025-01-09 21:46:23 +0530

    updated

---
+++ LargestNum.java
@@ -0,0 +1,12 @@
+public class LargestNum {
+    public static void main(String[] args) {
+        int a = 38;
+        int b = 243;
+
+        if(a>=b) {
+            System.err.println("A is largest of 2");
+        } else {
+            System.err.println("B is largest of 2");
+        }
+    }
+}
