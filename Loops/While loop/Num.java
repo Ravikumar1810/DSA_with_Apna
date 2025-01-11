b16f413b4f994b8fe06fba926d6ac2b3f32d5a67
Ravikumar1810 <nkravikumar18@gmail.com>
2025-01-12 01:11:39 +0530

    Loops

---
+++ Loops/While loop/Num.java
@@ -0,0 +1,14 @@
+package Loops.WhileLoop;
+
+public class Num {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 10)
+        {
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
