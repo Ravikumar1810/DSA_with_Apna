53b6c263f20da9ddf6ce81f9b68b9a8d65da59dd
Ravikumar1810 <nkravikumar18@gmail.com>
2025-01-17 00:06:45 +0530

    Basic program using methods

---
+++ Methods/BasicProg.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class BasicProg {
+
+    public static void PrintHelloWorld() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        PrintHelloWorld();
+    }
+}
