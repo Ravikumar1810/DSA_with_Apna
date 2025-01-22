426fa9f627312bf7f5e51414ddb4a9aab0243122
Ravikumar1810 <nkravikumar18@gmail.com>
2025-01-23 02:44:40 +0530

    inverted half pyramid with number

---
+++ Methods/inverted_half_py.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class inverted_half_py {
+    
+    public static void inverted_half_pyramid_num(int n) {
+        for(int i = 1; i<=n; i++) {
+
+            //inner loop
+            for(int j =1; j <= n-i+1; j++) {
+                System.out.print(j+ " ");
+            }
+
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        inverted_half_pyramid_num(6);
+    }
+}
