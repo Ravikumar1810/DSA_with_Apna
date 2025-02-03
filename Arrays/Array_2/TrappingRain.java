9e8e2a517d32d0724459c5a8e80289be08a1e7a6
Ravikumar1810 <nkravikumar18@gmail.com>
2025-02-03 16:37:11 +0530

    Trapping Rain Water

--- Arrays/Array_2/TrappingRain.java
+++ Arrays/Array_2/TrappingRain.java
@@ -1,11 +1,13 @@
+//Trapping Rain Water 
 package Arrays.Array_2;
 
 public class TrappingRain {
     public static int trappedrainwater(int height[]) {
+        //int height[] = {4,2,0,6,3,2,5}
         int n = height.length;
         int leftMax[] = new int[n];
         leftMax[0] = height[0];
-
+        
         for(int i = 1; i < n; i++) {
            leftMax[i]= Math.max(height[i], leftMax[i-1]);
         }
@@ -17,6 +19,7 @@ public class TrappingRain {
             rightMax[i]= Math.max(height[i], leftMax[i+1]);
          }
 
+         //loop
          int trappedWater = 0;
          for(int i = 0; i<n; i++) {
             int waterLevel = Math.min(leftMax[i], rightMax[i]);
