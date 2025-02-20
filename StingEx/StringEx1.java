16e86967e8c3ba5b3b249e1b43f583f7e9b42fcb
Ravikumar1810 <nkravikumar18@gmail.com>
2025-02-20 23:15:49 +0530

    StringBuilder updated

---
+++ StingEx/StringEx1.java
@@ -0,0 +1,15 @@
+package StingEx;
+
+public class StringEx1 {
+    public static void main(String[] args) {
+        StringBuilder s1 =new StringBuilder("ravikumar"); 
+        System.out.println(s1.length());
+        s1.append(" n k");
+        System.out.println(s1);
+    }
+
+}
+
+
+    
+
