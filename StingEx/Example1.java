342f14af2890a35ab8b356dc31116295c2aa7641
Ravikumar1810 <nkravikumar18@gmail.com>
2025-02-23 21:26:27 +0530

    String Concatination

---
+++ StingEx/Example1.java
@@ -0,0 +1,14 @@
+package StringExample;
+
+public class Example1 {
+    public static void main(String[] args) {
+        String s1 = "I am Ravikumar N K";
+        String s2 = "I Studing MCA";
+
+        // String s3 = s1 concat s2;
+
+        String s3 = s1 + " " + s2;
+        System.out.println(s3);
+    }
+}
+
