346125bb4ffff036038d0565ec67d25d201c586d
Ravikumar N K <nkravikumar18@gmail.com>
2025-04-03 23:35:03 +0530

    Update RightAngledTriangle.java

--- Patterns/RightAngledTriangle.java
+++ Patterns/RightAngledTriangle.java
@@ -13,7 +13,7 @@ public class RightAngledTriangle {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); // Move to the next row
+            System.out.println(); 
         }
     }
 }
